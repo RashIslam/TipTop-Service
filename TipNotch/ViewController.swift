b1bbde9dbbeb372b1cd38f339ba593a994aafce4
Rashadul Islam <rusewell@gmail.com>
2020-12-16 21:20:22 -0500

    Basic tip functionality - Props up keyboard when user taps on text field / closes when blank space is tapped -Added segmented control that changes tip percentages depending on prefixed tip amounts -Updates total based on segmented control

--- TipNotch/ViewController.swift
+++ TipNotch/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        //Retrieve Bill Amount
+        let bill = Double(billField.text!) ?? 0
+        //Calculate tip and total
+        let tipRanges = [0.15 , 0.18, 0.20]
+        let tip = bill * tipRanges[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        //Update totals on display
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
